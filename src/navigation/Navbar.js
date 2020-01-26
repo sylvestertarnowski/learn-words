a1a259115c127ac5cacc2ee163a28375a44cd3a3
Sylvester <34459277+sylvestertarnowski@users.noreply.github.com>
2020-01-26 19:31:19 +0100

    add new project

---
+++ src/navigation/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>Browse</li>
+        <li>Learn</li>
+        <li>Admin</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
